b441bfc4f9cb0ca373679fa8cd21dfedd25f3ca7
mlu <melodylu@users.noreply.github.com>
2015-10-21 19:31:38 -0700

    server test

--- server.js
+++ server.js
@@ -5,22 +5,45 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-/*
-  if we want to use views with a templating engine,
-  the commented out lines of code will start that.
-
-*/
-
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
- // response.render('pages/index');
- response.send('Go Fetch!')
+  response.render('pages/index');
 });
 
-var server = app.listen(app.get('port'), function() {
-  var host = server.address().address;
-  var port = server.address().port;
+app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+
+
+
+
+// var express = require('express');
+// var app = express();
+
+// app.set('port', (process.env.PORT || 5000));
+
+// app.use(express.static(__dirname + '/public'));
+
+// /*
+//   if we want to use views with a templating engine,
+//   the commented out lines of code will start that.
+
+// */
+
+// // app.set('views', __dirname + '/views');
+// // app.set('view engine', 'ejs');
+
+// app.get('/', function(request, response) {
+//  // response.render('pages/index');
+//  response.send('Go Fetch!')
+// });
+
+// var server = app.listen(app.get('port'), function() {
+//   var host = server.address().address;
+//   var port = server.address().port;
+//   console.log('Node app is running on port', app.get('port'));
+// });
