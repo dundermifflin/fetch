d7964cd6c229c00d9b02440d43e76674902904b5
mlu <melodylu@users.noreply.github.com>
2015-10-21 19:43:23 -0700

    arg server works

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  // response.render('pages/index');
+  response.send("Woof woof");
 });
 
 app.listen(app.get('port'), function() {
@@ -19,31 +20,7 @@ app.listen(app.get('port'), function() {
 
 
 
-
-
-// var express = require('express');
-// var app = express();
-
-// app.set('port', (process.env.PORT || 5000));
-
-// app.use(express.static(__dirname + '/public'));
-
-// /*
-//   if we want to use views with a templating engine,
-//   the commented out lines of code will start that.
-
-// */
-
-// // app.set('views', __dirname + '/views');
-// // app.set('view engine', 'ejs');
-
-// app.get('/', function(request, response) {
-//  // response.render('pages/index');
-//  response.send('Go Fetch!')
-// });
-
-// var server = app.listen(app.get('port'), function() {
-//   var host = server.address().address;
-//   var port = server.address().port;
-//   console.log('Node app is running on port', app.get('port'));
-// });
+/*
+  if we want to use views with a templating engine,
+  the commented out lines of code will start that.
+*/
