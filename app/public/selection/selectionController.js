b4201ee5af9f3ff9a1e52a4e92f0c82610e05709
mlu <melodylu@users.noreply.github.com>
2015-10-22 19:53:56 -0700

    Remove bootstrap links, add selectionController.js

--- app/public/selection/selectionController.js
+++ app/public/selection/selectionController.js
@@ -1,1 +1,11 @@
-// on click - >re-route to confirmation page
+angular.module('fetch.services')
+  .controller('SelectionController', ['$scope', '$state', function($scope, $state ) {
+
+    $scope.changeView = function(typeSelected) {
+      // Send the user to the confirmation page along with the kind of dog they selected.
+      $state.go('fetch.confirmation', {
+        type: typeSelected
+      });
+    };
+
+  }]);
