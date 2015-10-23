906b20b577ed38f728d319f6506bee589d4f3aa8
Rachel Lader <RachelLader@RachelsAir.local>
2015-10-23 08:30:05 -0700

    Add controllers to  states

--- app/public/app.js
+++ app/public/app.js
@@ -1,6 +1,6 @@
 //use $routeProvider or $uiRoute to navigate templates
 
-angular.module('fetch', ['ui.router'])
+angular.module('fetch', ['ui.router', 'fetch.login', 'fetch.selection', 'fetch.services', 'fetch.confirmation'])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
@@ -10,22 +10,32 @@ angular.module('fetch', ['ui.router'])
 
     .state('/login', {
         url: '/login',
+        controller:'authController',
         templateUrl: 'login.html'
       })
 
     .state('/logout', {
         url: '/login',
+        controller:'authController',
         templateUrl: 'login.html'
       })
 
     .state('/selection', {
       url: '/selection',
+      controller:'selectionController',
       templateUrl: 'selection.html'
     })
 
     .state('/confirmation', {
       url: '/confirmation',
+      controller:'confirmationController',
       templateUrl: 'confirmation.html'
+    })
+    .otherwise({
+      url:'/login',
+      controller:'authController',
+      templateUrl:'login.html'
+
     });
 
 });
