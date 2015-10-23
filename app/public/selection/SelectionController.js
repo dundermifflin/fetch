4be77dda1fce75bf597bd9fe324470d7c5c4b27d
Rachel Lader <RachelLader@RachelsAir.local>
2015-10-23 15:48:49 -0700

    Update confirmation to be a string and prettify format

--- app/public/selection/SelectionController.js
+++ app/public/selection/SelectionController.js
@@ -1,19 +1,20 @@
-
 angular.module('fetch.selection', [])
-  .controller('SelectionController', ['$scope', '$state','DogFactory' function($scope, $state, DogFactory) {
-  	$scope.data={};
- 
-     $scope.processSelection= function(typeSelected){
-     	//send type selected to DogFactory to process query in server
-     	DogFactory.processSelection(typeSelected).then(function(err,response){
-     		if(err){
-     			console.log('Error:',err);
-     		}
-     		//set data to be the dog that is sent back
-     		$scope.data= response;
-     		//redirect to the confirmation page, include data as a parameter
-     		$state.go(confirmation, {dog: $scope.data});
-     	})
+  .controller('SelectionController', ['$scope', '$state', 'DogFactory'function($scope, $state, DogFactory) {
+    $scope.data = {};
+
+    $scope.processSelection = function(typeSelected) {
+      //send type selected to DogFactory to process query in server
+      DogFactory.processSelection(typeSelected).then(function(err, response) {
+        if (err) {
+          console.log('Error:', err);
+        }
+        //set data to be the dog that is sent back
+        $scope.data = response;
+        //redirect to the confirmation page, include data as a parameter
+        $state.go('confirmation', {
+          dog: $scope.data
+        });
+      })
     };
 
   }]);
