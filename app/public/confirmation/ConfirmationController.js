f7570a42d5866fbeb9a5a6918eb70a0ee7f53692
Andrew Huang <andrew.huang12@gmail.com>
2015-10-23 15:56:15 -0700

    Set up ConfirmationController to receive data from SelectionController

--- app/public/confirmation/ConfirmationController.js
+++ app/public/confirmation/ConfirmationController.js
@@ -1,5 +1,13 @@
-//triggers factory function and pulls return value. Displays the values on the page. 
 angular.module('fetch.confirmation', [])
-  .controller('ConfirmationController', ['$scope', 'DogFactory', function($scope, DogFactory) {  
-    $scope.dog = DogFactory.findDog();	// what is arg?
-  }]);
+
+.controller('ConfirmationController', ['$scope', function($scope) {
+
+  // check to see what data is getting passed from SelectionController
+  console.log($state.params);
+
+  // should be 'dog' object. if so, set to $scope.dog
+  $scope.dog = $state.params;
+
+  // this will allow us to access dog object and its properties in confirmationView
+
+}])
