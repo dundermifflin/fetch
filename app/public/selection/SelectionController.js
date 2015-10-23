470cfab7f96062a6eb6c4715f9b182f2cdbe18e4
Rachel Lader <RachelLader@RachelsAir.local>
2015-10-23 15:42:39 -0700

    Add trigger in SelectionControl to talk to dog factory in processSelection function

--- app/public/selection/SelectionController.js
+++ app/public/selection/SelectionController.js
@@ -1,12 +1,19 @@
 
 angular.module('fetch.selection', [])
-  .controller('SelectionController', ['$scope', '$state', function($scope, $state ) {
-
-    $scope.changeView = function(typeSelected) {
-      // Send the user to the confirmation page along with the kind of dog they selected.
-      $state.go('fetch.confirmation', {
-        type: typeSelected
-      });
+  .controller('SelectionController', ['$scope', '$state','DogFactory' function($scope, $state, DogFactory) {
+  	$scope.data={};
+ 
+     $scope.processSelection= function(typeSelected){
+     	//send type selected to DogFactory to process query in server
+     	DogFactory.processSelection(typeSelected).then(function(err,response){
+     		if(err){
+     			console.log('Error:',err);
+     		}
+     		//set data to be the dog that is sent back
+     		$scope.data= response;
+     		//redirect to the confirmation page, include data as a parameter
+     		$state.go(confirmation, {dog: $scope.data});
+     	})
     };
 
   }]);
