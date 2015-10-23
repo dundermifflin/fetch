b19128a5b961f95df39a4d8a15664de805338e38
Rachel Lader <RachelLader@RachelsAir.local>
2015-10-23 15:41:14 -0700

    Add params to confirmation state in app.js to allow for data insertion

--- app/public/app.js
+++ app/public/app.js
@@ -27,7 +27,8 @@ angular.module('fetch', ['fetch.authorization', 'fetch.confirmation', 'fetch.sel
   })
 
   .state('confirmation', {
-      url: '/confirmation',
+      url: '/confirmation', //if this doesn't work, remove url line
+      params:['dog'], 
       controller: 'ConfirmationController',
       templateUrl: 'confirmation/confirmationView.html'
     });
