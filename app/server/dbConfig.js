af529cf6a78fdb66009d87c160301e161bd93518
Yusuf Modan <yumodan@gmail.com>
2015-10-25 22:59:29 -0700

    fix database
    updated database initialization and schema design syntax in accordance with newest version of bookshelf.
    
    added a database directory for sqlite to be able to store data in. server no longer crashes and database is no longer commented out. SOLID

--- app/server/dbConfig.js
+++ app/server/dbConfig.js
@@ -1,88 +1,86 @@
-// var path = require('path');
+var path = require('path');
+var knex = require('knex')({
+  client: 'sqlite',
+  connection: {
+    host: '127.0.0.1',
+    user: 'mksdundermifflin',
+    password: 'greenfield25',
+    database: 'fetch_db',
+    charset: 'utf8',
+    filename: path.join(__dirname, '../db/fetch.sqlite')
+  }
+});
 
-// var knex = require('knex')({
-//   client: 'mysql',
-//   connection: {
-//     host: '127.0.0.1',
-//     user: 'mksdundermifflin',
-//     password: 'greenfield25',
-//     database: 'fetch_db',
-//     charset: 'utf8'
-//   }
-// });
+knex.schema.hasTable('users').then(function(exists) {
+  if (!exists) {
+    knex.schema.createTable('users', function(user) {
+      user.increments('id').primary();
+      user.string('name');
+      user.string('password');
+      user.integer('zip');
+      user.binary('hasDog');
+    })
+  }
+});
 
+knex.schema.hasTable('dogs').then(function(exists) {
+  if (!exists) {
+    knex.schema.createTable('dogs', function(dog) {
+      dog.increments('id').primary();
+      dog.integer('userId')
+        .unsigned()
+        .references('id')
+        .inTable('users');
+      dog.integer('shelterId')
+        .unsigned()
+        .references('id')
+        .inTable('shelters');
+      dog.binary('isMale');
+      dog.string('blurb');
+      dog.string('activity');
+      dog.string('photoUrl');
+      dog.string('breed');
+    })
+  }
+});
 
-// var db = require('bookshelf')(knex);
-
-
-// db.knex.schema.hasTable('users').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('users', function(user) {
-//       user.increments('id').primary();
-//       user.string('name');
-//       user.string('password');
-//       user.integer('zip');
-//       user.binary('hasDog');
-//     })
-//   }
-// });
-
-// db.knex.schema.hasTable('dogs').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('dogs', function(dog) {
-//       dog.increments('id').primary();
-//       dog.integer('userId')
-//         .unsigned()
-//         .references('id')
-//         .inTable('users');
-//       dog.integer('shelterId')
-//         .unsigned()
-//         .references('id')
-//         .inTable('shelters');
-//       dog.binary('isMale');
-//       dog.string('blurb');
-//       dog.string('activity');
-//       dog.string('photoUrl');
-//       dog.string('breed');
-//     })
-//   }
-// });
-
-// db.knex.schema.hasTable('shelters').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('shelters', function(shelter) {
-//       shelter.increments('id').primary();
-//       shelter.integer('zip')
-//       shelter.integer('name')
-//     })
-//   }
-// });
+knex.schema.hasTable('shelters').then(function(exists) {
+  if (!exists) {
+    knex.schema.createTable('shelters', function(shelter) {
+      shelter.increments('id').primary();
+      shelter.integer('zip')
+      shelter.integer('name')
+    })
+  }
+});
 
 
 
 
-// //BOOKSHELF MODEL COLLECTION ABSTRACTIONS BELOW:
+//BOOKSHELF MODEL COLLECTION ABSTRACTIONS BELOW:
 
-// module.exports.User = db.Model.extend({
-//   tableName: 'users',
-//   dog: function() {
-//     return this.hasOne(Dog);
-//   }
-// });
+var bookshelf = require('bookshelf')(knex)
 
-// module.exports.Dog = db.Model.extend({
-//   tableName: 'dogs',
-//   shelter: function() {
-//     return this.belongsTo(Shelter);
-//   },
-//   user: function() {
-//     return this.belongsTo(User);
-//   }
-// });
+module.exports.User = bookshelf.Model.extend({
+  tableName: 'users',
+  dog: function() {
+    return this.hasOne(Dog);
+  }
+});
 
-// module.exports.Shelter = db.Model.extend({
-//   tableName: 'shelters',
-//   dog: function() {
-//     return this.hasMany(Dog);
-//   }
-// });
+module.exports.Dog = bookshelf.Model.extend({
+  tableName: 'dogs',
+  shelter: function() {
+    return this.belongsTo(Shelter);
+  },
+  user: function() {
+    return this.belongsTo(User);
+  }
+});
+
+module.exports.Shelter = bookshelf.Model.extend({
+  tableName: 'shelters',
+  dog: function() {
+    return this.hasMany(Dog);
+  }
+});
