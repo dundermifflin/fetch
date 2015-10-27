bdb3949019903286acbfc87666c2d495d7743f40
Rachel Lader <RachelLader@RachelsAir.local>
2015-10-27 11:37:57 -0700

    Update selectionView ng-show

--- app/public/selection/SelectionController.js
+++ app/public/selection/SelectionController.js
@@ -7,6 +7,11 @@ angular.module('fetch.selection', [])
     console.log('in process selection hurray')
       //send type selected to DogFactory to process query in server
 
+    $scope.hover= function(activity){
+      console.log('hoveringgggg')
+      return $scope.activity.show===true;
+    }
+
     $scope.data = DogFactory.processSelection(typeSelected).then(function(response) {
       console.log('selection response', response.data)
       $state.go('confirmation', {
