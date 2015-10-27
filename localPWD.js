3f4f6f8270f0c95f3045dd21b3d08ef2fa07ac82
mlu <melodylu@users.noreply.github.com>
2015-10-27 13:04:14 -0700

    Fight the localPWD.js fight

--- localPWD.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  host: "ec2-107-21-219-142.compute-1.amazonaws.com",
-  port: "5432",
-  user: "zjplyeljrfseij",
-  password: "7oNFzi5iaSuixF6kqYyyRLQM14",
-  database: "d7f6r0ms273te6",
-  ssl: true
-};
