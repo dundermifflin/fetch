7b6ef0429dfef490c958bc2e28c4865f6bc9239c
Rachel Lader <RachelLader@RachelsAir.local>
2015-10-29 10:30:54 -0700

    Delete extra if statement

--- app/public/confirmation/ConfirmationController.js
+++ app/public/confirmation/ConfirmationController.js
@@ -3,7 +3,13 @@ angular.module('fetch.confirmation', [])
 .controller('ConfirmationController', ['$scope', '$state', 'uiGmapGoogleMapApi', function($scope, $state, uiGmapGoogleMapApi) {
 
   uiGmapGoogleMapApi.then(function(maps) {
-    $scope.map = { center: { latitude: 37.8, longitude: -122.4 }, zoom: 13 };
+    $scope.map = {
+      center: {
+        latitude: 37.8,
+        longitude: -122.4
+      },
+      zoom: 13
+    };
   });
 
   $scope.marker = {
@@ -20,14 +26,12 @@ angular.module('fetch.confirmation', [])
 
   // should be 'dog' object. if so, set to $scope.dog
   $scope.dog = JSON.parse($state.params.dog);
-    console.log('Male', $scope.dog)
-    if (($scope.dog.isMale === true) || ($scope.dog.isMale===null)) {
-    console.log('Male', $scope.dog.isMale)
-    if ($scope.dog.isMale === true ) {
-      $scope.dog.gender = 'his'
-    } else {
-      $scope.dog.gender = 'her'
-    }
-  // this will allow us to access dog object and its properties in confirmationView
+  console.log('Male', $scope.dog)
+  if (($scope.dog.isMale === true) || ($scope.dog.isMale === null) || ($scope.dog.isMale === 1)) {
+    $scope.dog.gender = 'his'
+  } else {
+    $scope.dog.gender = 'her'
   }
+  // this will allow us to access dog object and its properties in confirmationView
+
 }]);
